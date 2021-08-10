bb20dec7aaa0f10ba7fc0e93073a9c4b9c6fb40d
kcherkasov <kcherkasov@emphasoft.com>
2021-08-10 11:02:01 +0300

    Minute component and all its files created

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hour from "../Hour";
+import Minute from "../Minute";
 import "./Clock.scss";
 
 const Clock: React.FC = () => {
   return (
     <div className="Clock">
       <Hour />
+      <Minute />
     </div>
   );
 };
---
+++ src/components/Minute/Minute.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Minute.scss";
+
+const Minute: React.FC = () => {
+  return (
+    <div className="Minute">
+      <div className="Minute__mn" id="mn"></div>
+    </div>
+  );
+};
+
+export default Minute;
