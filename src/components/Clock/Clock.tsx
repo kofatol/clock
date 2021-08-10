9c21cb0c3972e08d07fe4ddb25ab7ece90933462
kcherkasov <kcherkasov@emphasoft.com>
2021-08-10 11:04:45 +0300

    Second component and all its files created

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hour from "../Hour";
 import Minute from "../Minute";
+import Second from "../Second";
 import "./Clock.scss";
 
 const Clock: React.FC = () => {
@@ -8,6 +9,7 @@ const Clock: React.FC = () => {
     <div className="Clock">
       <Hour />
       <Minute />
+      <Second />
     </div>
   );
 };
---
+++ src/components/Second/Second.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Second.scss";
+
+const Second: React.FC = () => {
+  return (
+    <div className="Second">
+      <div className="Second__sc" id="sc"></div>
+    </div>
+  );
+};
+
+export default Second;
