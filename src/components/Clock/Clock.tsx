62a28e3522794d45dc60db30cebba7b4e19242fa
kcherkasov <kcherkasov@emphasoft.com>
2021-08-10 10:58:16 +0300

    Hour component and all it's files created

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Hour from "../Hour";
 import "./Clock.scss";
 
 const Clock: React.FC = () => {
-  return <div className="clock">Clock</div>
+  return (
+    <div className="Clock">
+      <Hour />
+    </div>
+  );
 };
 
 export default Clock;
---
+++ src/components/Hour/Hour.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hour.scss";
+
+const Hour: React.FC = () => {
+  return (
+  <div className="Hour">
+    <div className="Hour__hr" id="hr"></div>
+  </div>
+  )
+};
+
+export default Hour;
