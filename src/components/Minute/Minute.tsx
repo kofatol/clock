4a8f52aaa877bb3fe12689b032d0383b73f845b8
kcherkasov <kcherkasov@emphasoft.com>
2021-08-11 21:01:30 +0300

    added component styling logic to Minute component

--- src/components/Minute/Minute.tsx
+++ src/components/Minute/Minute.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import ClockChild from "../../interfaces/ClockChild";
 import "./Minute.scss";
 
-const Minute: React.FC = () => {
+const Minute: React.FC<ClockChild> = ({ turnDegree }) => {
   return (
     <div className="Minute">
-      <div className="Minute__mn" id="mn"></div>
+      <div
+        className="Minute__mn"
+        style={{ transform: `rotateZ(${turnDegree}deg)` }}
+      ></div>
     </div>
   );
 };
