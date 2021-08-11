34b2305991cd73a8861cb791722f3408e75c253d
kcherkasov <kcherkasov@emphasoft.com>
2021-08-11 21:02:51 +0300

    added component styling logic to Second component

--- src/components/Second/Second.tsx
+++ src/components/Second/Second.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import ClockChild from "../../interfaces/ClockChild";
 import "./Second.scss";
 
-const Second: React.FC = () => {
+const Second: React.FC<ClockChild> = ({ turnDegree }) => {
   return (
     <div className="Second">
-      <div className="Second__sc" id="sc"></div>
+      <div
+        className="Second__sc"
+        style={{ transform: `rotateZ(${turnDegree}deg)` }}
+      ></div>
     </div>
   );
 };
