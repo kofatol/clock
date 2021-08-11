43a4593a3f5bc2a40a316ede9825e0ba05528a18
kcherkasov <kcherkasov@emphasoft.com>
2021-08-11 21:00:33 +0300

    created js logic in Clock component

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Hour from "../Hour";
 import Minute from "../Minute";
 import Second from "../Second";
 import "./Clock.scss";
 
 const Clock: React.FC = () => {
+  const deg = 6;
+  const [hh, setHh] = useState(0);
+  const [mm, setMm] = useState(0);
+  const [ss, setSs] = useState(0);
+
+  setInterval(() => {
+    const day = new Date();
+    setHh(() => day.getHours() * 30);
+    setMm(() => day.getMinutes() * deg);
+    setSs(() => day.getSeconds() * deg);
+  }, 500);
+
   return (
     <div className="Clock">
-      <Hour />
-      <Minute />
-      <Second />
+      <Hour turnDegree={hh + mm / 12} />
+      <Minute turnDegree={mm} />
+      <Second turnDegree={ss} />
     </div>
   );
 };
