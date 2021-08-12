d6e55d729e13c97b3310f85ef27e9d5ecb4384cf
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 23:18:42 +0300

    theme switching logic added to Clock component

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useTime } from "./hooks";
 import Hour from "../Hour";
 import Minute from "../Minute";
 import Second from "../Second";
 import Spinner from "../Spinner";
+import { AppContext } from "../../App.model";
 import "./Clock.scss";
 
 const Clock: React.FC = () => {
   const { hh, mm, ss } = useTime();
+  const { isLightMode } = useContext(AppContext);
 
   return (
     <>
       {!hh ? (
         <Spinner />
       ) : (
-        // <div className="Clock Clock_dark">
-        <div className="Clock Clock_light">
+        <div
+          className={`Clock
+          ${isLightMode ? "Clock_light" : "Clock_dark"}`}
+        >
           <Hour rotationDegree={hh + mm / 12} />
           <Minute rotationDegree={mm} />
           <Second rotationDegree={ss} />
