cb2d31a4c7fceda19a5b5b33c302e15a6e470f9a
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 11:09:59 +0300

    added show Spinner logic to Clock component

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -3,17 +3,24 @@ import { useTime } from "./hooks";
 import Hour from "../Hour";
 import Minute from "../Minute";
 import Second from "../Second";
+import Spinner from "../Spinner";
 import "./Clock.scss";
 
 const Clock: React.FC = () => {
   const { hh, mm, ss } = useTime();
 
   return (
-    <div className="Clock">
-      <Hour rotationDegree={hh + mm / 12} />
-      <Minute rotationDegree={mm} />
-      <Second rotationDegree={ss} />
-    </div>
+    <>
+      {!hh ? (
+        <Spinner />
+      ) : (
+        <div className="Clock">
+          <Hour rotationDegree={hh + mm / 12} />
+          <Minute rotationDegree={mm} />
+          <Second rotationDegree={ss} />
+        </div>
+      )}
+    </>
   );
 };
 
