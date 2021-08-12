9d810eee61d8e3849209e05744399fe402efa5e3
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 21:51:16 +0300

    created Minute__mn_dark & Minute__mn_light css styles

--- src/components/Minute/Minute.tsx
+++ src/components/Minute/Minute.tsx
@@ -6,7 +6,8 @@ const Minute: React.FC<ClockChild> = ({ rotationDegree }) => {
   return (
     <div className="Minute">
       <div
-        className="Minute__mn"
+        // className="Minute__mn Minute__mn_dark"
+        className="Minute__mn Minute__mn_light"
         style={{ transform: `rotateZ(${rotationDegree}deg)` }}
       ></div>
     </div>
