efefe8a4e2308fc16b80fc92bd02d6e8ac496e51
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 23:25:51 +0300

    theme switching logic added to Second component

--- src/components/Second/Second.tsx
+++ src/components/Second/Second.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../../App.model";
 import { ClockChildProps } from "../../interfaces";
 import "./Second.scss";
 
 const Second: React.FC<ClockChildProps> = ({ rotationDegree }) => {
+  const { isLightMode } = useContext(AppContext);
+
   return (
     <div className="Second">
       <div
-        // className="Second__sc Second__sc_dark"
-        className="Second__sc Second__sc_light"
+        className={`Second__sc
+          ${isLightMode ? "Second__sc_light" : "Second__sc_dark"}`}
         style={{ transform: `rotateZ(${rotationDegree}deg)` }}
       ></div>
     </div>
