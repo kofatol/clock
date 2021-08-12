9fd27824f7a11ba0e793abd07a47f9f841d059e0
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 23:22:02 +0300

    theme switching logic added to Minute component

--- src/components/Minute/Minute.tsx
+++ src/components/Minute/Minute.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../../App.model";
 import { ClockChildProps } from "../../interfaces";
 import "./Minute.scss";
 
 const Minute: React.FC<ClockChildProps> = ({ rotationDegree }) => {
+  const { isLightMode } = useContext(AppContext);
+
   return (
     <div className="Minute">
       <div
-        // className="Minute__mn Minute__mn_dark"
-        className="Minute__mn Minute__mn_light"
+        className={`Minute__mn
+          ${isLightMode ? "Minute__mn_light" : "Minute__mn_dark"}`}
         style={{ transform: `rotateZ(${rotationDegree}deg)` }}
       ></div>
     </div>
