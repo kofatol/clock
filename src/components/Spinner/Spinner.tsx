56259966955555ce43f5f8936cdf2795345f0d59
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 11:09:25 +0300

    Spinner component and all its files created

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Spinner.scss";
+
+const Spinner: React.FC = () => {
+  return (
+    <div className="Spinner">
+      <div className="Spinner__container">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
