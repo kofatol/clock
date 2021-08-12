5442f9a2e3bae2f9360cc95c241a30d71c390e21
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 23:34:51 +0300

    theme changing logic removed from Second component

--- src/components/Second/Second.tsx
+++ src/components/Second/Second.tsx
@@ -1,16 +1,12 @@
 import React, { useContext } from "react";
-import { AppContext } from "../../App.model";
 import { ClockChildProps } from "../../interfaces";
 import "./Second.scss";
 
 const Second: React.FC<ClockChildProps> = ({ rotationDegree }) => {
-  const { isLightMode } = useContext(AppContext);
-
   return (
     <div className="Second">
       <div
-        className={`Second__sc
-          ${isLightMode ? "Second__sc_light" : "Second__sc_dark"}`}
+        className="Second__sc"
         style={{ transform: `rotateZ(${rotationDegree}deg)` }}
       ></div>
     </div>
