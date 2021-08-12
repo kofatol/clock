0359fef560a65e1df864c31e0e59e32e0f46bc7b
kcherkasov <kcherkasov@emphasoft.com>
2021-08-12 22:51:00 +0300

    onThemeChange func created; ThemeSwitcherProps interface created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Clock from "./components/Clock";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import "./App.scss";
 
 const App: React.FC = () => {
+  const [isLightMode, setIsLightMode] = useState(false);
+
+  const onThemeChange = () => {
+    setIsLightMode((prevState) => !prevState);
+    console.log("clicked");
+  };
+
   return (
     // <div className="App App_dark">
     <div className="App App_light">
-      <ThemeSwitcher />
+      <ThemeSwitcher onClick={onThemeChange} />
       <Clock />
     </div>
   );
--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./ThemeSwitcher.scss";
 
-const ThemeSwitcher: React.FC = () => {
+interface ThemeSwitcherProps {
+  onClick: () => void;
+}
+
+const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({ onClick }) => {
   return (
-    <div className="ThemeSwitcher ThemeSwitcher_dark">
-    {/* <div className="ThemeSwitcher ThemeSwitcher_light"> */}
+    <div className="ThemeSwitcher ThemeSwitcher_dark" onClick={onClick}>
+      {/* <div className="ThemeSwitcher ThemeSwitcher_light"> */}
       <span className="ThemeSwitcher__text">Dark mode</span>
     </div>
   );
